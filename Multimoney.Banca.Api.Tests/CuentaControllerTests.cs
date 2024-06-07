2f7772b99d45210c9027b0c6cd1db8ac8729f5f9
fjos807 <31530117+fjos807@users.noreply.github.com>
2024-06-07 01:59:32 -0600

    Agrega proyecto de pruebas funcionales

---
+++ Multimoney.Banca.Api.Tests/CuentaControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace Multimoney.Banca.Api.Tests
+{
+    public class CuentaControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpCliente;
+
+        public CuentaControllerTests(WebApplicationFactory<Program> app)
+        {
+            _httpCliente = app.CreateClient();
+        }
+
+        [Fact]
+        private async void ConsultarCuentaExitoso()
+        {
+            var cuerpoPrueba = new
+            {
+                tipoIdentificacion = 1,
+                identificacion = "100010001",
+                numeroCuenta = 1
+            };
+            var stringContenido = new StringContent(JsonConvert.SerializeObject(cuerpoPrueba), Encoding.UTF8, "application/json");
+
+            var respuesta = await _httpCliente.PostAsync("/api/v1/cuentas", stringContenido);
+
+            respuesta.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        private async void ConsultarCuentaFallido()
+        {
+            var cuerpoPrueba = new
+            {
+                tipoIdentificacion = 1,
+                identificacion = "hola",
+                numeroCuenta = 1
+            };
+            var stringContenido = new StringContent(JsonConvert.SerializeObject(cuerpoPrueba), Encoding.UTF8, "application/json");
+
+            var respuesta = await _httpCliente.PostAsync("/api/v1/cuentas", stringContenido);
+
+            respuesta.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ Multimoney.Banca.Api.Tests/DepositosControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Multimoney.Banca.Api.Tests
+{
+    public class DepositosControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpCliente;
+
+        public DepositosControllerTests(WebApplicationFactory<Program> app)
+        {
+            _httpCliente = app.CreateClient();
+        }
+
+        [Fact]
+        private async void RealizarDepositoExitoso()
+        {
+            var cuerpoPrueba = new
+            {
+                tipoIdentificacion = 1,
+                identificacion = "100010001",
+                numeroCuenta = 1,
+                monto = 3500
+            };
+            var stringContenido = new StringContent(JsonConvert.SerializeObject(cuerpoPrueba), Encoding.UTF8, "application/json");
+
+            var respuesta = await _httpCliente.PostAsync("/api/v1/cuentas/depositos", stringContenido);
+
+            respuesta.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        private async void RealizarDepositoFallido()
+        {
+            var cuerpoPrueba = new
+            {
+                tipoIdentificacion = 1,
+                identificacion = "hola",
+                numeroCuenta = 1,
+                monto = 3500
+            };
+            var stringContenido = new StringContent(JsonConvert.SerializeObject(cuerpoPrueba), Encoding.UTF8, "application/json");
+
+            var respuesta = await _httpCliente.PostAsync("/api/v1/cuentas/depositos", stringContenido);
+
+            respuesta.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
